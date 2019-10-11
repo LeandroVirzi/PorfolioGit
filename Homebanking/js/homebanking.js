3152f8828fcd62d77e8a05f38cbb77d4f3b00275
leandro virzi <leandro.m.virzi@gmail.com>
2019-10-10 21:45:25 -0300

    Correcciones Homebanking

--- Homebanking/js/homebanking.js
+++ Homebanking/js/homebanking.js
@@ -28,61 +28,48 @@ function extraerDinero() {
     var extraer = prompt("monto que quieres retirar: ");//extraer el dinero.
     var billetes_cien = 100;//validar que el monto pueda ser extraido en billetes de 100.
     
+
+
     //validar que el monto pueda ser extraido en billetes de 100.
-    while (extraer % 100 > 0 ){
-        alert("El monto minimo es de $100");
-        extraer = prompt("Ingrese 0 para salir. \n Ingrese nuevamente el monto: ");
-        if (extraer == 0){
-            break;
-        }
+    if (extraer % 100 > 0 ){
+        alert("El monto minimo es de $100");    
     }
     
-    while (extraer > limiteExtraccion){
+    //si supera el limite de extraccion.
+    else if (extraer > limiteExtraccion){
         alert("Estas superando el limite diario: "+limiteExtraccion);
-        extraer = prompt("Ingrese 0 para salir. \n Ingrese nuevamente el monto: ");
-        if (extraer == 0){
-            break;
-        }
     }
 
     //validar el saldo de la cuenta.
-    while(extraer > saldoCuenta ){
+    else if (extraer > saldoCuenta ){
         alert("El monto es mayor al Saldo de la cuenta. \n Saldo: " + saldoCuenta)
-        extraer = prompt("Ingrese 0 para salir. \n Ingrese nuevamente el monto: ")
-        if (extraer == 0){
-            break;
-        }
     }
 
     //validar que no supere el limite de extraccion.
-    while(extraer > limiteExtraccion ){
+    else if (extraer > limiteExtraccion ){
         alert("El monto supera el limite de extraccion. \n Limite: "+limiteExtraccion);
-        extraer = prompt("Ingrese 0 para salir. \n Ingrese nuevamente el monto: ");
-        if (extraer == 0){
-            break;
-        }
-    }
+    } 
 
     //validar billetes de 100.
-    while(extraer > 0 && extraer < 100 ){
+    else if (extraer > 0 && extraer < 100 ){
         alert("El monto minimo es de $100");
-        extraer = prompt("Ingrese 0 para salir. \n Ingrese nuevamente el monto: ");
-        if (extraer == 0){
-            break;
-        }
+    } 
+    else {
+        //validar que ccuando extrae dinero no supere el limite de extraccion diaria.
+        limiteExtraccion = limiteExtraccion- extraer;
+        //alert("Limite de extraccion: "+limiteExtraccion+"\n Extraccion diaria: "+limiteExtraccionDiaria);
+    
+        //saldo de la cuenta actualizado.
+        saldoCuenta = parseInt(saldoCuenta) - parseInt(extraer);//a extraer. 
+        alert("Has extraido: "+extraer + "\n Saldo anterior: " +saldoAnterior+ "\n Saldo actual: " +saldoCuenta+ " Limite de extraccion: " +limiteExtraccion);//mensaje de saldo.
+        
+        //actualizar vista.
+        actualizarSaldoEnPantalla();
+        actualizarLimiteEnPantalla();
+        
     }
-
-    //validar que ccuando extrae dinero no supere el limite de extraccion diaria.
-    limiteExtraccion = limiteExtraccion- extraer;
-    //alert("Limite de extraccion: "+limiteExtraccion+"\n Extraccion diaria: "+limiteExtraccionDiaria);
-    
-    
-
-    
-    //saldo de la cuenta actualizado.
-    saldoCuenta = parseInt(saldoCuenta) - parseInt(extraer);//a extraer. 
-    alert("Has extraido: "+extraer + "\n Saldo anterior: " +saldoAnterior+ "\n Saldo actual: " +saldoCuenta+ " Limite de extraccion: " +limiteExtraccion);//mensaje de saldo.
 }
+    
 
 //sumar dinero a la cuenta.
 function depositarDinero() {
@@ -90,10 +77,57 @@ function depositarDinero() {
     var deposito = prompt("Ingrese el saldo del dinero a depositar: ");//toma el saldo a ingresar.
     saldoCuenta = parseInt(saldoCuenta) + parseInt(deposito); //actualiza el saldo de la cuenta.
     alert("Has depositado: "+deposito + "\n Saldo anterior: "+saldoAnterior + "\n Saldo actual: "+saldoCuenta);//deposito.
+    actualizarSaldoEnPantalla();
+    actualizarLimiteEnPantalla();
 }
 
 function pagarServicio() {
+    //variables.
+    var agua = 350;//1
+    var telefono = 425;//2
+    var luz = 210;//3
+    var internet = 570;//4
 
+    //elija servicio a pagar.
+    var servicioElegido = prompt("Ingrese el número que corresponda con el servicio que quieres pagar \n1 - Agua \n2 - Luz \n3 - Internet \n4 - Teléfono");
+    if (servicioElegido == null || servicioElegido == ""){
+        alert("No eligio un servicio");
+    } else{
+        let servicio = parseInt(servicioElegido);
+        switch (servicio) {
+            case 1:
+                alert("sasasd");
+                //debugger
+                realizarPagoServicio("Agua",agua);
+                break;
+            case 2:
+                realizarPagoServicio("Teléfono",telefono);
+                break;
+            case 3:
+                realizarPagoServicio("Luz",luz);
+                break;
+            case 4:
+                realizarPagoServicio("Internet",internet);
+                break;
+            default: alert("No existe el servicio seleccionado.");
+                break;    
+        }
+        actualizarSaldoEnPantalla();
+    }
+    
+}
+
+function realizarPagoServicio(servicio,pagarServicio){
+    //si tiene el saldo en la cuenta, haga lo siguiente.
+    //debugger
+    if (pagarServicio < saldoCuenta){
+        let saldoAnterior = saldoCuenta;
+        saldoCuenta = saldoCuenta - pagarServicio;
+        alert("Has pagado: "+servicio +"\nImporte: "+pagarServicio+ "\nSaldo anterior: " +saldoAnterior+ "\nSaldo actual: " +saldoCuenta);//mensaje de saldo.
+    } else {
+        alert("No tienes dinero suficiente para cubrir este gasto.")
+    }
+    actualizarSaldoEnPantalla();
 }
 
 function transferirDinero() {
