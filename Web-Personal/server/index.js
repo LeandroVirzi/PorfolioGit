e6c41adf5a939bdae0430a332f396afb67edc8d8
leandro virzi <leandro.m.virzi@gmail.com>
2020-03-31 19:21:42 -0300

    Actualizacion

--- Web-Personal/server/index.js
+++ Web-Personal/server/index.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const port = process.env.PORT || 3977;//importamos el puerto.
+const port = process.env.PORT || 9998;//importamos el puerto. el puerto 3977 estaba siendo utilizado se reemplazo.
 const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");//version de la api.
 
 //coneccion a la base de datos. 
- 
+
+mongoose.set("useFindAndModify", false);//solucionar error DeprecationWarning.
+
 //no toma las varicables API_VERSION, IP_SERVER, PORT_DB,
 //lo subi a mano localhost:27017 REVISAR.
 //mongoose.connect('mongodb://${IP_SERVER}:${PORT_DB}/leandrovirzibd'
@@ -21,9 +23,8 @@ mongoose.connect('mongodb://localhost:27017/leandrovirzibd' ,
             console.log("######################");
             console.log("##### API REST #######");
             console.log("######################");
-            //console.log('http://localhost:3977/api/v1/');
-            console.log('http://${IP_SERVER}:${PORT_DB}/api/${API_VERSION}/');
+            console.log('http://localhost:9998/api/v1/');
+            //console.log('http://${IP_SERVER}:${PORT_DB}/api/${API_VERSION}/');
         })
     }
-});
-
+}); 
